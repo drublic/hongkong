5885dc571e87601b9eee173f690ea0a855c09c15
Hans Christian Reinl <info@drublic.de>
2014-10-08 23:20:44 +0200

    Add testing with Jasmine and JSHint for code style

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+/**
+ * General Grunt setup
+ */
+'use strict';
+
+/*
+ * Call Grunt configuration
+ */
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  });
+
+  // Load project configuration
+  grunt.initConfig({
+    jshint: {
+      all: [
+        '*.js',
+        'test/**/*.js'
+      ],
+      options: {
+          jshintrc: '.jshintrc'
+      }
+    },
+    jasmine: {
+      js: {
+        src: [
+          'bower_components/jquery/dist/jquery.min.js',
+          'bower_components/raf.js/raf.min.js',
+          'hongkong.js'
+        ],
+        options: {
+          specs: 'test/specs/*.spec.js'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  /**
+   * Testing
+   */
+  grunt.registerTask('test', [
+    'jasmine:unit'
+  ]);
+
+  /**
+   * Travis CI task
+   */
+  grunt.registerTask('travis', [
+    'jshint',
+    'jasmine:unit'
+  ]);
+
+};
